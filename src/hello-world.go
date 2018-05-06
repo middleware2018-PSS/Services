0ea8c7ed52f7a9b7d7ac97097775fee41d15d323
Philippe scorsolini <pscorso93@gmail.com>
2018-05-06 10:06:30 +0200

    test

--- src/hello-world.go
+++ src/hello-world.go
@@ -1,11 +1,35 @@
 package main
 
-
-import "github.com/gin-gonic/gin"
-import "fmt"
-import "net/http"
+import (
+	"github.com/middleware2018-PSS/Services/src/models"
+	"fmt"
+	"encoding/json"
+	"time"
+	"github.com/gobuffalo/pop"
+	"log"
+)
 
 func main() {
-    fmt.Println("hello world")
+	student := models.Student{}
+	student.Payments = []models.Payment{{0, 100, true, time.Now(), ""}}
+	student.Appointments = []models.Appointment{
+		{
+			Time:time.Now(),
+			Location:"",
+			Teacher: models.Teacher{},
+		},
+	}
+	str, _ := json.MarshalIndent(student," ", "  ")
+	str2, _ := json.MarshalIndent(models.Teacher{}," ", "  ")
+	fmt.Printf("%s\n",str)
+	fmt.Printf("%s\n",str2)
+	fmt.Println(time.Now().Date())
+	db, err := pop.Connect("development")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer db.Close()
+	fmt.Println(db.URL())
+
 }
 
